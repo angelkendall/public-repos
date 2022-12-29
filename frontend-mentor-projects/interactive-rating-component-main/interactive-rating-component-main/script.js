5e22fa4a181fa187011fea631e3957d801b4f1b5
angel <angelkendall13@gmail.com>
2022-12-29 20:57:12 +1100

    Added submit button functionality

--- frontend-mentor-projects/interactive-rating-component-main/interactive-rating-component-main/script.js
+++ frontend-mentor-projects/interactive-rating-component-main/interactive-rating-component-main/script.js
@@ -1,14 +1,27 @@
-// getting element by class name gives an array, not an object
+// store the rating buttons as an array (not object)
 const ratingButtons = document.getElementsByClassName("btn-rating");
+
+// store the two state divs 
+const ratingState = document.getElementById("rating-state");
+const thankyouState = document.getElementById("thankyou-state");
+
+// declare empty variable to store the rating
 let rating;
 
 // loop through the array of btn-rating class buttons
 for (var i = 0; i < ratingButtons.length; i++){
 // when creating an addEventListener you can use the event object to target the element clicked
     ratingButtons[i].addEventListener("click", function (event) {
+        // set the variable to equal the corresponding button rating
         rating = event.target.innerHTML;
         console.log(rating);
+        // display the variable to HTML
+        document.getElementById("chosen-rating").innerHTML = rating;
     })
 }
 
-
+// make the rating state hidden, make the thankyou state visible 
+function changeStates() {
+    ratingState.style.display = "none";
+    thankyouState.style.display = "flex";
+}
